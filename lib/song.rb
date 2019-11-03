cf58860ac1cd02cf40a716ddac38d684fddd36ea
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-03 18:59:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,42 +1,42 @@
 require 'pry'
 
 class Song
-  attr_accessor :name, :artist, :genre
+  # attr_accessor :name, :artist, :genre
   
-  @@count = 0
-  @@genres = []
-  @@artists = []
+  # @@count = 0
+  # @@genres = []
+  # @@artists = []
 
-  def initialize(name, artist, genre) 
-    @name = name
-    @artist = artist 
-    @genre = genre 
-    @@count += 1 
-    @@artists.push(artist) 
-    @@genres.push(genre) 
-  end
+  # def initialize(name, artist, genre) 
+  #   @name = name
+  #   @artist = artist 
+  #   @genre = genre 
+  #   @@count += 1 
+  #   @@artists.push(artist) 
+  #   @@genres.push(genre) 
+  # end
 
-  def self.count
-    @@count
-  end
+  # def self.count
+  #   @@count
+  # end
   
-  def self.genres
-    @unique_genres = []
+  # def self.genres
+  #   @unique_genres = []
     
-    @@genres.map do |genre|
-      @unique_genres << genre
-      unless @unique_genres.include?(genre)
-    end
-    @unqiue_genres
-  end
+  #   @@genres.map do |genre|
+  #     @unique_genres << genre
+  #     unless @unique_genres.include?(genre)
+  #   end
+  #   @unqiue_genres
+  # end
   
-  def self.artists
-    @unique_artists = []
+  # def self.artists
+  #   @unique_artists = []
     
-    @@artists.map do |artist| 
-      @unique_artists << artist
-      unless @unique_artists.include?(artist)
-    end
-    @unique_artists
-  end
+  #   @@artists.map do |artist| 
+  #     @unique_artists << artist
+  #     unless @unique_artists.include?(artist)
+  #   end
+  #   @unique_artists
+  # end
 end
