210668575f186738990d3c7c0bce4c9e5b968a41
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-03 19:00:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,20 @@
 require 'pry'
 
 class Song
-  # attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist, :genre
   
-  # @@count = 0
-  # @@genres = []
-  # @@artists = []
+  @@count = 0
+  @@genres = []
+  @@artists = []
 
-  # def initialize(name, artist, genre) 
-  #   @name = name
-  #   @artist = artist 
-  #   @genre = genre 
-  #   @@count += 1 
-  #   @@artists.push(artist) 
-  #   @@genres.push(genre) 
-  # end
+  def initialize(name, artist, genre) 
+    @name = name
+    @artist = artist 
+    @genre = genre 
+    @@count += 1 
+    @@artists.push(artist) 
+    @@genres.push(genre) 
+  end
 
   # def self.count
   #   @@count
